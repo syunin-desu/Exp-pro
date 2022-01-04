535ad05ae9fb6f8a902efb5fc17cbf62265c87db
yusakuNakano <yuregin.syunin@gmail.com>
2022-01-04 23:21:43 +0900

    add Button in Battle

---
+++ Assets/Scripts/Quest/BattleUIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BattleUIManager : MonoBehaviour
+{
+
+    public Button attackButton;
+    public Button abilityButton;
+    public Button defenceButton;
+    public Button itemButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
