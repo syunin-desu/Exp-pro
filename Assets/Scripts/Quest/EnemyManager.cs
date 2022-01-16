5e9f9bf7f4158141a75a217c226660831340a1df
yusakuNakano <yuregin.syunin@gmail.com>
2022-01-16 21:47:02 +0900

    add defence_action and sort_action

--- Assets/Scripts/Quest/EnemyManager.cs
+++ Assets/Scripts/Quest/EnemyManager.cs
@@ -7,8 +7,14 @@ using UnityEngine;
 //char_Baseから継承
 public class EnemyManager : CharBase
 {
-
+    // TODO UIManagerもプロパティに含めないか検討
+    // Attackメソッドをオーバーライドし、その中でUIの値の更新も行う
     Action tapAction; //クリックされた時に実行したい関数
+
+    void Start()
+    {
+        this.char_role = Const.CO.ENEMY;
+    }
 
     public void AddEventListenerOnTap(Action action)
     {
--- Assets/Scripts/Quest/PlayerManager.cs
+++ Assets/Scripts/Quest/PlayerManager.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class PlayerManager : CharBase
 {
+    // TODO UIManagerもプロパティに含めないか検討
+    // Attackメソッドをオーバーライドし、その中でUIの値の更新も行う
 
+    void Start()
+    {
+        this.char_role = Const.CO.PLAYER;
+    }
 }
